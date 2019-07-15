41ef7c414c9ba01beac356bed0de5bc98147898d
xinglong1 <xinglong1@jd.com>
2019-07-15 15:40:35 +0800

    init jdcloud-wsgi-wrapper

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="jdcloud_wsgi_wrapper",
+    version="1.0",
+    description="jdcloud wsgi wrapper",
+    author="function",
+
+    py_modules=['headers','constants','jdcloud_wsgi_wrapper','file_wrapper','example'],
+    install_requires=[],
+)
+
